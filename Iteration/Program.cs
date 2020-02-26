f79f94a21957f77468d005fac835fcc1b53a848f
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-02-26 16:31:56 -0700

    Add project files.

---
+++ Iteration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Page 137
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] > 85)
+                {
+                    Console.WriteLine("Passing test score " + testScores[i]);
+                }
+            }
+
+            string[] names = { "Jesse", "Ryan", "Bob Guy", "Adam" };
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "Jesse")
+                {
+                    Console.WriteLine(names[i]);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
